b57ba64964d4c848ee72ef2b7566f9dd258d16ef
Mathilde <111729553+mathildeolesen@users.noreply.github.com>
2024-01-17 17:35:54 +0100

    Forsøk på resourcescript
    Må finne ut hvordan man genererer basert på trykket navigasjon på nettsiden, så javascript koden er kommentert ut for nå.

---
+++ resourcescript.js
@@ -0,0 +1,21 @@
+/*
+let resourceHTML = ""
+
+resources.map(resource => {
+    let sourcesList = ""; 
+
+    for (let i = 0; i < resource.sources.length; i++) {
+        sourceList += `<li><a href="${resource.sources[i].url}">${resource.sources[i].title}</a></li>`;
+    }
+
+    resourceHTML += `<article>
+        <h2>${resource.category}</h2>
+        <p>${resource.text}</p>
+        <ul>${sourceList}</ul>
+    </article>`;
+});
+
+const main = document.getElementsByTagName("main")
+main[0].innerHTML = resourceHTML
+
+*/
