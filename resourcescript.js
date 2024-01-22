53007c4054c87008dadaee74cf183031f8cae65e
Mathilde <111729553+mathildeolesen@users.noreply.github.com>
2024-01-22 20:38:18 +0100

    Lagt til funksjonalitet for menyer og innlasting av riktig ressurs ved valg
    resourcescript.js skal være så og si ferdigstilt.
    Usikker på bruken av <button id="sanity and headless cms">, pga valg av id-navn, men for nå er dette bra nok.

--- resourcescript.js
+++ resourcescript.js
@@ -1,21 +1,52 @@
-/*
-let resourceHTML = ""
 
-resources.map(resource => {
-    let sourcesList = ""; 
+    const navbuttons = document.querySelectorAll("nav ul li button");
+    const main = document.getElementsByTagName("main")
 
-    for (let i = 0; i < resource.sources.length; i++) {
-        sourceList += `<li><a href="${resource.sources[i].url}">${resource.sources[i].title}</a></li>`;
+    let resourceHTML = ""
+    
+    function updateArticle(resource) {
+
+        let sourcesList = resource.sources.map(source => `<li><a href="${source.url}">${source.title}</a></li>`).join('');
+
+        resourceHTML = `<article>
+            <h2>${resource.category}</h2>
+            <p>${resource.text}</p>
+            <ul>${sourcesList}</ul>
+            </article>
+        `;
+
+        main[0].innerHTML = resourceHTML
     }
 
-    resourceHTML += `<article>
-        <h2>${resource.category}</h2>
-        <p>${resource.text}</p>
-        <ul>${sourceList}</ul>
-    </article>`;
-});
+    function selectCategory(categoryId) {
+        const resource = resources.find(res => res.category.toLowerCase() === categoryId.toLowerCase());
+        // .toLowerCase : Gjør om alt til små bokstaver så de kan sammenlignes. I ressurser er kategoriene med Store og små bokstaver, 
+        // mens html class og id bruker små bokstaver (ifølge best practice).
+        if (resource) {
+            updateArticle(resource);
+        }
 
-const main = document.getElementsByTagName("main")
-main[0].innerHTML = resourceHTML
+         // Fjern "selected" klasse fra alle button
+         navbuttons.forEach(button => {
+            button.classList.remove("selected");
+        });
 
-*/
+        // Legg til "selected" klasse til den trykte button
+        const activeButton = document.getElementById(categoryId);
+        if (activeButton) {
+            activeButton.classList.add("selected");
+        }
+    }
+
+    // Sett opp event listeners for alle button
+    navbuttons.forEach(button => {
+        button.addEventListener("click", () => {
+            selectCategory(button.id);
+        });
+    });
+
+    
+    // Setter HTML som startvalg ved start
+    selectCategory("html");
+    
+
